13a547fc01e4122378ae46872285e6214e9cff73
Muhammad Noaman <muhammadnoaman345@gmail.com>
2025-09-26 09:57:56 +0500

    Create button.jsx

---
+++ components/ui/button.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { cn } from "../../lib/utils"
+
+export const Button = React.forwardRef(
+  ({ className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          "px-4 py-2 rounded-lg font-medium bg-blue-600 text-white hover:bg-blue-700 transition",
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
