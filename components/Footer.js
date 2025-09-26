48d63b74c6810ea898c234020b8dfe32435ebc83
Muhammad Noaman <muhammadnoaman345@gmail.com>
2025-09-26 11:23:34 +0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar.js";
+import Footer from "../components/Footer.js";
 
 export default function Checkout() {
   const [formData, setFormData] = useState({
