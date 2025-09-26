f47a7719a36e516f835ca3c97d2d9233d644888d
Muhammad Noaman <muhammadnoaman345@gmail.com>
2025-09-26 17:27:37 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
-export default function App({ Component, pageProps }){
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+
+      {/* ✅ Tawk.to Live Chat */}
+      <Script
+        id="tawk-to"
+        src="https://embed.tawk.to/68bde50d67c586192c66aa63/1j4isdfct"
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
