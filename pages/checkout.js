3d16a2745aaacb0b06d7f7ed87dddf7361c2debf
Muhammad Noaman <muhammadnoaman345@gmail.com>
2025-09-26 11:35:43 +0500

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -43,7 +43,7 @@ export default function Checkout() {
       </div>
 
       {/* Main Content */}
-      <main className="flex-1 max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <main className="flex-1 max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 lg:grid-cols-3 gap-8 mt-24">
         
         {/* Left: Form */}
         <div className="lg:col-span-2 bg-white shadow rounded-2xl p-8 border border-gray-100">
